58b27cd25ee54c4348ab7b5dcf5ccb47143f74be
Musa22623 <MusabinOsman220623@outlook.com>
2026-03-11 16:21:57 +0800

    [feat] Security Features        - JWT Authentication        - Token based authorization        - Secure REST API

---
+++ src/main/java/com/musa/payment/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.musa.payment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+}
--- src/main/java/com/musa/payment/entity/User.java
+++ src/main/java/com/musa/payment/entity/User.java
@@ -1,4 +1,24 @@
 package com.musa.payment.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Getter
+@Setter
 public class User {
+
+    @jakarta.persistence.Id
+    private Long id1;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+
 }
